c6781e1879642c2705b7b6d94705ab89d324fbe0
Bilal Mughal <bilalxmughal@gmail.com>
2026-05-02 19:23:05 +0500

    Use env variables for Firebase config

--- src/firebase/config.js
+++ src/firebase/config.js
@@ -1,17 +1,8 @@
-import { initializeApp } from "firebase/app";
-import { getAnalytics } from "firebase/analytics";
-import { getFirestore } from "firebase/firestore"; // ← ADD THIS
-
 const firebaseConfig = {
-  apiKey: "AIzaSyDwxdT1uWHQugaEAwUZaBjbJg648jLQ254",
-  authDomain: "buscaro-route-planner.firebaseapp.com",
-  projectId: "buscaro-route-planner",
-  storageBucket: "buscaro-route-planner.firebasestorage.app",
-  messagingSenderId: "1044244610595",
-  appId: "1:1044244610595:web:79b496e715b88b69a2bb96",
-  measurementId: "G-TP7PLJ2LN5"
-};
-
-const app = initializeApp(firebaseConfig);
-const analytics = getAnalytics(app);
-export const db = getFirestore(app); // ← ADD THIS
+  apiKey:            process.env.REACT_APP_FIREBASE_API_KEY,
+  authDomain:        process.env.REACT_APP_FIREBASE_AUTH_DOMAIN,
+  projectId:         process.env.REACT_APP_FIREBASE_PROJECT_ID,
+  storageBucket:     process.env.REACT_APP_FIREBASE_STORAGE_BUCKET,
+  messagingSenderId: process.env.REACT_APP_FIREBASE_MESSAGING_ID,
+  appId:             process.env.REACT_APP_FIREBASE_APP_ID,
+}
